ccb62949f5439f7eb853e7aedf84f04b4664e7c9
Alexander Goroshnikov <rogsc@mail.ru>
2019-12-01 18:54:33 +0500

    test commit

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?
+//comment for test commit
+
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetPageProperty("title", "Демонстрационная версия продукта «1С-Битрикс: Управление сайтом»");
 $APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
