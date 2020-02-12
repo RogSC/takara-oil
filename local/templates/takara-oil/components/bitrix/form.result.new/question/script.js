859f86845d59741675bf537bdc61d057291dfaa6
Alexander Goroshnikov <rogsc@mail.ru>
2020-02-12 08:38:10 +0500

    add lang in forms scripts

--- local/templates/takara-oil/components/bitrix/form.result.new/question/script.js
+++ local/templates/takara-oil/components/bitrix/form.result.new/question/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
+    let form = $('[data-id="6"]'),
+        msg = [];
 
-    let form = $('[data-id="6"]');
+    if ($('html').attr('lang') === 'ru') {
+        msg['request'] = 'Вопрос успешно отправлен';
+    } else if ($('html').attr('lang') === 'en') {
+        msg['request'] = 'Question sent successfully';
+    } else {
+        msg['request'] = '質問を送信しました';
+    }
 
     form.off('submit.ajax-form').on('submit.ajax-form', function (e) {
         e.preventDefault();
@@ -23,7 +31,7 @@ $(document).ready(function () {
                         $('#callback__errors').append("<div class='modal__error'>" + res['message'] + "</div>");
                     }
                     if (res.hasOwnProperty('result') && res['result'] === true) {
-                        let result = '<div class="modal-window"><h4 class="">Вопрос успешно отправлен</h4></div>';
+                        let result = '<div class="modal-window"><h4 class="">' + msg['request'] + '</h4></div>';
                         $.arcticmodal({
                             content: result
                         });
